d7829fe796ab0321f4c99475c6a1309ffc8887e5
GoMinBong <sjy2017z@naver.com>
2020-06-08 19:48:26 +0900

    cc1c1c123123

--- LinkedList/Stack.cpp
+++ LinkedList/Stack.cpp
@@ -23,59 +23,80 @@ void Push(Stack* stack, Node_stack* NewNode) {
 }
 Node_stack* Pop(Stack* stack) {
 	Node_stack* PopNode = stack->tail;
-}
-int FreeStack(Stack* stack) {
-	char data;
-	Node_stack* Temp;
-	if (IsEmpty(stack)) {
-		printf("Asd");
-		return 0;
-	}
-	Temp = stack->top;
-	data = Temp->data;
-	
-	stack->top = Temp->next;
-	free(Temp);
-	return data;
 
+	if (stack->top == stack->tail) {
+		stack->top = NULL;
+		stack->tail = NULL;
+	}
+	else {
+		Node_stack* current = stack->top;
+		while (current->next != PopNode) {
+			current = current->next;
+		}
+		stack->tail = current;
+		stack->tail->next = NULL;
+	}
+	return PopNode;
+}
+void FreeStack(Stack* stack ) {
+	while (!IsEmpty(stack)) {
+		Node_stack* PopNode = Pop(stack);
+		free(PopNode->data);
+		free(PopNode);
+	}
+}
+Node_stack* GetTop(Stack* stack) {
+	return stack->tail;
+}
+bool isEmpty(Stack* stack) {
+	return (stack->top == NULL && stack->tail == NULL);
 }
 
 Node_stack* CreateNode(char* NewData) {
 	Node_stack* NewNode = (Node_stack*)malloc(sizeof(Node_stack));
 
 	NewNode->data = (char*)malloc(sizeof(strlen(NewData) + 1));
-	strcpy(NewNode->data, NewData);
+	strcpy_s(NewNode->data,8, NewData);
 	NewNode->next = NULL;
 
 	return NewNode;
 }
 
+unsigned int GetPriority(char op, int inStack) {
+	unsigned int priority = -1;
+	switch (op) {
+	case LEFT_PARENTHESES:
+		if (inStack) {
+			priority = 3;
+		}
+		else {
+			priority = 0;
+		} break;
+	case MULTIPLY:
+	case DIVIDE:
+		priority = 1; break;
+	case PLUS:
+	case MINUS:
+		priority = 2; break;
+	}
+	return priority;
+}
+
+bool IsPrior(char op1, char op2) {
+	return (GetPriority(op1, 1) < GetPriority(op2, 0));
+}
+
+bool IsNum(char c) {
+	for (int i = 0; i < 11; i++) {
+		if (num[i] == c) {
+			return true;
+		}
+	}
+	return false;
+}
 void Start1() {
 	Stack stack;
 	
 	InitStack(&stack);
 
-	char num[11] = { '0','1','2','3','4','5','6','7','8','9','.', };
-	enum symbol {
-		LEFT_PARENTHESES = '(',
-		RIGHT_PARENTHESES = ')',
-		MULTIPLY = '*',
-		DIVIDE = '/',
-		PLUS = '+',
-		MINUS = '-',
-		SPACE = ' ',
-		OPERAND
-	};
-	while (1) {
-		printf("계산할 값 입력");
-		
-	}
-
-	Push(&stack,'1'); 
-	Push(&stack,'2');
-	Push(&stack,'3');
-
-	while (!IsEmpty(&stack)) {
-		printf("asdasd%d", Pop(&stack));
-	}
 }
--- LinkedList/Stack.h
+++ LinkedList/Stack.h
@@ -1,7 +1,6 @@
 #pragma once
 #ifndef STACK_H
 #define STACK_H
-
 void Start1();
 typedef struct node_stack {
 	char* data;
@@ -12,5 +11,19 @@ typedef struct stack {
 	Node_stack* top;
 	Node_stack* tail;
 }Stack;
+
+
+char num[11] = { '0','1','2','3','4','5','6','7','8','9','.', };
+
+enum symbol {
+	LEFT_PARENTHESES = '(',
+	RIGHT_PARENTHESES = ')',
+	MULTIPLY = '*',
+	DIVIDE = '/',
+	PLUS = '+',
+	MINUS = '-',
+	SPACE = ' ',
+	OPERAND
+};
 
 #endif
