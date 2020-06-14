d957629b49ceb4e440af661c951c85056fe577bb
Gominbong <sjy2017z@naver.com>
2020-06-14 21:03:57 +0900

    3333

--- LinkedList/Stack.cpp
+++ LinkedList/Stack.cpp
@@ -93,7 +93,7 @@ void PostfixConversion(char exp[]) {
 			ConvExp[index++] = comparison;
 		}
 		else if (comparison == '(' || comparison == ')') {
-			printf("hi");
+
 		}
 		else{
 			ConvExp[index++] = ' ';
